64247eebc0d63033ceba9949082024385088ee8a
Lise <lise.carpenter@gmail.com>
2021-04-27 16:56:39 +0700

    clean up

--- src/App.js
+++ src/App.js
@@ -4,10 +4,8 @@ import './App.css'
 function App() {
 	return (
 		<div className='App'>
-			<header className='App-header'>
-				<img src={burger} className='App-logo' alt='burger' />
-				<p>This is going to be an app about burgers</p>
-			</header>
+			<img src={burger} className='App-logo' alt='burger' />
+			<p>This is going to be an app about burgers</p>
 		</div>
 	)
 }
