c5a6756a53a46ffc6972392e1f2a3aed30ca2a42
Lise <lise.carpenter@gmail.com>
2021-04-28 14:02:34 +0700

    styling

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -3,10 +3,15 @@ import React, { FunctionComponent } from 'react'
 const About: FunctionComponent = () => {
 	return (
 		<>
-			<h1>About Burgers</h1>
-			<p>The goal of this site is to maintain a regular list of where to find delicious burgers in Bangkok.</p>
-			<h3>Why?</h3>
-			<p>Because burgers are the best food, obviously.</p>
+			<div className='container'>
+				<h1>About Burgers</h1>
+				<p>
+					The goal of this site is to maintain a regular list of where to find
+					delicious burgers in Bangkok.
+				</p>
+				<h3>Why?</h3>
+				<p>Because burgers are the best food, obviously.</p>
+			</div>
 		</>
 	)
 }
--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -3,8 +3,10 @@ import React, { FunctionComponent } from 'react'
 const Contact: FunctionComponent = () => {
 	return (
 		<>
-			<h1>Want to tell us about a new burger place?</h1>
-			<p>Email: lise.carpenter@gmail.com</p>
+			<div className='container'>
+				<h1>Want to tell us about a new burger place?</h1>
+				<p>Email: lise.carpenter@gmail.com</p>
+			</div>
 		</>
 	)
 }
