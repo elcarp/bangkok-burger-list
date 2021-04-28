68a6786255b60a98c2556fc68427b7b5987711fc
Lise <lise.carpenter@gmail.com>
2021-04-28 17:20:37 +0700

    format and style

--- src/components/burger_card.tsx
+++ src/components/burger_card.tsx
@@ -4,24 +4,39 @@ import data from '../api/burger_places.json'
 const burgerPlaces = data.burgerPlaces
 
 const BurgerCard: FunctionComponent = () => {
-	const burgerData = burgerPlaces.map(({ name, location, price_range, ranking, delivery, tags, description }: any) => {
-		return (
-			<>
-				<div className='bg-white rounded-lg text-left w-1/2 mt-10 p-4'>
-					<div className='w-3/4 grid grid-cols-2'>
-						<label>Name</label> {name}
-						<label>Location</label> {location}
-                        <label>Price</label> {price_range}
-                        <label>Ranking</label> {ranking}
-                        <label>Delivery</label> {delivery}
-                        <label>Tags</label> {tags}
-                        <label>Description</label> {description}
-
+	const burgerData = burgerPlaces.map(
+		({
+			name,
+			location,
+			price_range,
+			ranking,
+			delivery,
+			tags,
+			description,
+		}: any) => {
+			return (
+				<>
+					<div className='bg-white rounded-lg text-left w-1/2 mt-10 p-4'>
+						<div className='w-3/4 grid grid-cols-2'>
+							<label>Name</label> <span className='capitalize'>{name}</span>
+							<label>Location</label>{' '}
+							<span className='capitalize'>{location.join(', ')}</span>
+							<label>Price</label>{' '}
+							<span className='capitalize'>{price_range}</span>
+							<label>Ranking</label>{' '}
+							<span className='capitalize'>{ranking}</span>
+							<label>Delivery</label>{' '}
+							<span className='capitalize'>{delivery.join(', ')}</span>
+							<label>Tags</label>{' '}
+							<span className='capitalize'>{tags.join(', ')}</span>
+							<label>Description</label>{' '}
+							<span className='capitalize'>{description}</span>
+						</div>
 					</div>
-				</div>
-			</>
-		)
-	})
+				</>
+			)
+		}
+	)
 	return <>{burgerData}</>
 }
 
