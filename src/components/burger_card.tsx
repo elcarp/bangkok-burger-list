d5b832f6e89cf0ef6a4de7823b703e998f4ba37e
Lise <lise.carpenter@gmail.com>
2021-04-29 09:52:18 +0700

    create filter options

--- src/components/burger_card.tsx
+++ src/components/burger_card.tsx
@@ -7,7 +7,27 @@ const BurgerCard: FunctionComponent = () => {
 	const locationOptions = burgerPlaces.map(({ location }) => {
 		return { label: location, value: location }
 	})
+	const priceOptions = [
+		{ label: '$', value: '$' },
+		{ label: '$$', value: '$$' },
+		{ label: '$$$', value: '$$$' },
+		{ label: '$$$$', value: '$$$$' },
+	]
+	const deliveryOption = [
+		{ label: 'Food Panda', value: 'food_panda' },
+		{ label: 'LINE MAN', value: 'line_man' },
+		{ label: 'GrabFood', value: 'grab_food' },
+		{ label: 'GoJek', value: 'go_jek' },
+    ]
+    const tagOptions = burgerPlaces.map(({ tags }) => {
+		return { label: tags, value: tags }
+	})
 	const [selectedLocation, setSelectedLocation] = useState<any>()
+	const [selectedPriceRange, setSelectedPriceRange] = useState<any>()
+    const [selectedDeliveryOption, setSelectedDeliveryOption] = useState<any>()
+    const [selectedTagOptions, setSelectedTagOptions] = useState<any>()
+
+
 
 	const burgerData = burgerPlaces.map(
 		({ name, location, price_range, ranking, delivery, tags, description }) => {
@@ -36,13 +56,40 @@ const BurgerCard: FunctionComponent = () => {
 	)
 	return (
 		<>
-			<Select
-				value={selectedLocation}
-				required={true}
-				options={locationOptions}
-				onChange={setSelectedLocation}
-				placeholder='Select Currency'
-			/>
+			<div className='filter-wrapper w-full md:w-3/4 justify-center mt-10 block md:flex mx-auto'>
+				<Select
+					value={selectedLocation}
+					required={true}
+					options={locationOptions}
+					onChange={setSelectedLocation}
+					placeholder='Location'
+					className='text-left w-1/4 z-50 mx-4'
+				/>
+				<Select
+					value={selectedPriceRange}
+					required={true}
+					options={priceOptions}
+					onChange={setSelectedPriceRange}
+					placeholder='Price'
+					className='text-left w-1/4 z-50 mx-4'
+				/>
+				<Select
+					value={selectedDeliveryOption}
+					required={true}
+					options={deliveryOption}
+					onChange={setSelectedDeliveryOption}
+					placeholder='Delivery'
+					className='text-left w-1/4 z-50 mx-4'
+				/>
+				<Select
+					value={selectedTagOptions}
+					required={true}
+					options={tagOptions}
+					onChange={setSelectedTagOptions}
+					placeholder='Tags'
+					className='text-left w-1/4 z-50 mx-4'
+				/>
+			</div>
 			{burgerData}
 		</>
 	)
