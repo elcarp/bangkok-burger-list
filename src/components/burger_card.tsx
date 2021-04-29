7e3be7a06e91ee009cd8e1e42fe86ec72d0d5213
Lise <lise.carpenter@gmail.com>
2021-04-29 09:41:38 +0700

    install react select

--- src/components/burger_card.tsx
+++ src/components/burger_card.tsx
@@ -1,20 +1,16 @@
 import React, { FunctionComponent, useState } from 'react'
+import Select from 'react-select'
 import data from '../api/burger_places.json'
 
 const burgerPlaces = data.burgerPlaces
 const BurgerCard: FunctionComponent = () => {
+	const locationOptions = burgerPlaces.map(({ location }) => {
+		return { label: location, value: location }
+	})
 	const [selectedLocation, setSelectedLocation] = useState<any>()
 
 	const burgerData = burgerPlaces.map(
-		({
-			name,
-			location,
-			price_range,
-			ranking,
-			delivery,
-			tags,
-			description,
-		}: any) => {
+		({ name, location, price_range, ranking, delivery, tags, description }) => {
 			return (
 				<>
 					<div className='bg-white rounded-lg text-left w-full md:w-1/2 mt-10 p-4 shadow-lg mx-auto'>
@@ -38,7 +34,18 @@ const BurgerCard: FunctionComponent = () => {
 			)
 		}
 	)
-	return <>{burgerData}</>
+	return (
+		<>
+			<Select
+				value={selectedLocation}
+				required={true}
+				options={locationOptions}
+				onChange={setSelectedLocation}
+				placeholder='Select Currency'
+			/>
+			{burgerData}
+		</>
+	)
 }
 
 export default BurgerCard
