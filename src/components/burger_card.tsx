645984956ba4531d9abaf2b00c225be790c454e3
Lise <lise.carpenter@gmail.com>
2021-05-12 09:42:10 +0700

    clean up

--- src/components/burger_card.tsx
+++ src/components/burger_card.tsx
@@ -4,33 +4,6 @@ import data from '../api/burger_places.json'
 
 const burgerPlaces = data.burgerPlaces
 
-const allBurgerData = burgerPlaces.map(
-		({ name, location, price_range, ranking, delivery, tags, description }) => {
-			return (
-				<>
-					<div className='bg-white rounded-lg text-left w-full md:w-1/2 mt-10 p-4 shadow-lg mx-auto'>
-						<div className='w-full md:w-3/4 grid grid-cols-2'>
-							<label>Name</label> <span className='capitalize'>{name}</span>
-							<label>Location</label>{' '}
-							<span className='capitalize'>{location}</span>
-							<label>Price</label>{' '}
-							<span className='capitalize'>{price_range}</span>
-							<label>Ranking</label>{' '}
-							<span className='capitalize'>{ranking}</span>
-							<label>Delivery</label>{' '}
-							<span className='capitalize'>{delivery.join(', ')}</span>
-							<label>Tags</label>{' '}
-							<span className='capitalize'>{tags.join(', ')}</span>
-							{/* <label>Description</label>{' '} */}
-							{/* <span className='capitalize'>{description}</span> */}
-						</div>
-					</div>
-				</>
-			)
-		}
-	)
-
-
 const BurgerCard: FunctionComponent = () => {
 
 	const locationOptions = [
