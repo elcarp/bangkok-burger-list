be1df7a3b87f18983ce6b981c050f126001c4093
Lise <lise.carpenter@gmail.com>
2021-05-12 08:06:03 +0700

    styling

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react'
+import  { FunctionComponent } from 'react'
 import BurgerCard from '../../components/burger_card'
 
 const Home: FunctionComponent = () => {
